9c7dc658379dd965ca0057916c4c558e8544ac6e
Siwapol Techaratsami <SIWAPOLT@cementhai.com>
2019-09-06 10:31:50 +0700

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=False)
